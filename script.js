bf00f206131a20c2dcb53da942f11de1dc7839ca
Cody Kimb <codykimbell@gmail.com>
2020-12-18 16:27:14 -0600

    input variables and arrays of choices

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 // Assignment code here
 
+// User input variables
+var userInput;
+var confirmNum;
+var confirmChar;
+var confirmUpper;
+var confirmLower;
+
+// arrays of choices
+characters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+alphaLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var toUpper = function(x) {
+  return x.toUpperCase();
+};
+
+alphaUp = alphaLow.map(toUpperCase);
+
+var userChoices;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
